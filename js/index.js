96e27a373bad844a4e6c4a557815b42a995f9acb
Coder206 <patrick.arrow206@outlook.com>
2016-03-29 10:48:53 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 var argent;
 var pointage;
 
+var button = document.getElementById("b");
+
 argent = document.getElementById("a").textContent;
-alert(argent);
+
+function nButton() {
+    var btn = document.createElement("BUTTON");
+    document.body.appendChild(btn);
+}
+
+button.addEventListener("click", nButton);
