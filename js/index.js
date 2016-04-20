e088bb5d4eb0f537f897ebff5c012587651a8e83
Coder206 <patrick.arrow206@outlook.com>
2016-04-19 21:51:58 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -65,3 +65,46 @@ function trigger() {
 
 
 window.addEventListener("load", initUI);
+
+
+//Decisions js
+
+//DOM
+var a = document.getElementById("a").textContent;
+var sys = document.getElementById("sys");
+var serie = document.getElementById("serie");
+
+//Valeurs
+var oargent;
+var argent;
+
+function gagnant() {
+    //Multiplier la valeur par 0.4
+    argent = argent * 0.4
+    //Décision continuer
+    if (confirm("Voulez-vous essayer vos chances encore?") == true) {
+        sys = "Charger";
+    } else {
+        sys = "gagnant";
+    }
+}
+
+function perdant() {
+    //Multiplier la valeur par 0.45
+    argent = argent * 0.45
+    //Pas d'argent?
+    if (argent <= 0) {
+        sys = "perdant";
+    }
+    else {
+        //Décision continuer
+        if (confirm("Voulez-vous essayer vos chances encore?") == true) {
+            sys = "Charger";
+        } else {
+            sys = "perdant";
+        }
+    }
+}
+
+document.querySelector(".x").addEventListener("click", gagnant);
+document.querySelectorAll(".bu").addEventListener("click", gagnant);
