fe191f9cf1bbf73f122556f484a915cc2079914f
Coder206 <patrick.arrow206@outlook.com>
2016-04-25 14:10:35 -0400

    Added files via upload

--- js/index.js
+++ js/index.js
@@ -1,134 +1,116 @@
 //Variables - argent 
-var a; 
-var argent; 
-var pointage = 1; 
- 
+var a;
+var argent;
+var pointage = 1;
+
 //Variables - jeu 
-var tours = 3; 
-var numr = 0; 
- 
+var tours = 3;
+var numr = 0;
+
 //DOM 
-a = document.getElementById("a").textContent; 
-sys = document.getElementById("sys"); 
-serie = document.getElementById("serie"); 
+a = document.getElementById("a").textContent;
+sys = document.getElementById("sys").textContent;
+serie = document.getElementById("serie");
 
 //Défini argent 
-argent = a; 
-pointage = a * 100; 
- 
+argent = a;
+pointage = a * 100;
+
 //Créer un bouton 
-function nButton(nombre) { 
-    for (var i = 0; i < tours; i++) { 
-        if (i === numr) { 
-            var btn = document.createElement("BUTTON"); 
-            btn.className = "x"; 
-            btn.id = "b" + i; 
-            serie.appendChild(btn); 
-        } 
-        else { 
-            var btn = document.createElement("BUTTON"); 
-            btn.className = "bu"; 
-            btn.id = "b" + i; 
+function nButton(nombre) {
+    for (var i = 0; i < tours; i++) {
+       var btn = document.createElement("BUTTON");
+        if (i === numr) {
+            btn.className = "x";
+            btn.id = "b";
             serie.appendChild(btn);
-        } 
-    } 
-} 
- 
- 
+        }
+        else {
+            btn.className = "bu";
+            btn.id = "b";
+            serie.appendChild(btn);
+        }
+    }
+}
+
+
 //Enlèver un boutton 
-function rButton(nombre) { 
-    var parent = serie.getElementById("serie"); 
-    var child = document.getElementById("b"); 
-    parent.removeChild(child); 
-} 
- 
- 
-function initUI() { 
-    rnd(); 
-    nButton(numr); 
-    trigger(); 
-} 
+function rButton() {
+    var parent = serie.getElementById("serie");
+    var child = document.getElementById("b");
+    parent.removeChild(child);
+}
+
+
+function initUI() {
+    rnd();
+    nButton(numr);
+    trigger();
+}
 //Choisi un nombre entier au hazard entre 1 et tours 
-function rnd() { 
-    numr = Math.floor((Math.random() * tours) + 0); 
-} 
+function rnd() {
+    numr = Math.floor((Math.random() * tours) + 0);
+}
 //Commence rnd 
-function start() { 
-    rnd(); 
-} 
+function start() {
+    rnd();
+}
 //Commence start si pointage et plus grand ou égal à 100 
-function trigger() { 
-    if (pointage >= 100) { 
-        start(); 
-    } 
-} 
- 
-//Decisions js 
- 
-//DOM 
-//var a = document.getElementById("a").textContent; 
-//var sys = document.getElementById("sys"); 
-//var serie = document.getElementById("serie"); 
- 
- 
-//Valeurs 
-//var oargent; 
-var argent; 
- 
- 
-function gagnant() { 
+function trigger() {
+    if (pointage >= 100) {
+        start();
+    }
+}
+
+function gagnant() {
     //Multiplier la valeur par 0.4 
-    argent = argent * 0.4 
+    argent = argent * 0.4;
     //Décision continuer 
- 
- 
-    var r = confirm("Voulez-vous essayer vos chances encore?"); 
-    if (r == true) { 
-        sys = "Charger"; 
-        tours++; 
-        numr++; 
-        initUI(); 
-    } else { 
-        sys = "Fermer"; 
-    } 
-} 
- 
- 
-function perdant() { 
+    var r = confirm("Voulez-vous essayer vos chances encore?");
+    if (r ===true) {
+        sys = "Charger";
+        tours++;
+        numr++;
+        initUI();
+    } else {
+        sys = "Fermer";
+    }
+}
+
+function perdant() {
     //Multiplier la valeur par 0.45 
-    argent = argent * 0.45 
+    argent = argent * 0.45;
     //Pas d'argent? 
-    if (argent <= 0) { 
-        sys = "Fermer"; 
- 
- 
-    } 
-    else { 
+    if (argent <= 0) {
+        sys = "Fermer";
+    }
+    else {
         //Décision continuer 
-        var r = confirm("Voulez-vous essayer vos chances encore?"); 
-        if (r == true) { 
-            sys = "Charger"; 
-            tours--; 
-            numr--; 
-            initUI(); 
-        } else { 
-            sys = "Fermer"; 
-        } 
-    } 
-} 
- 
+        var r = confirm("Voulez-vous essayer vos chances encore?");
+        if (r === rue) {
+            sys = "Charger";
+            tours--;
+            numr--;
+            initUI();
+        } else {
+            sys = "Fermer";
+        }
+    }
+}
+
 //Verification 
-function ver() { 
-    if (this.className = "x") { 
-        gagnant(); 
-    } 
-    else { 
-        perdant(); 
-    } 
-} 
- 
-window.addEventListener("load", initUI); 
-document.getElementsByTagName("button", ver); 
- 
- 
- 
+function ver() {
+    alert("HI!");
+    if (this.className === "x") {
+        gagnant();
+        alert("g");
+    }
+    else {
+        perdant();
+        alert("p");
+    }
+}
+
+window.addEventListener("load", initUI);
+//document.getElementsByClassName("bu").addEventListener("click", ver);
+document.getElementById("b").addEventListener("click", ver);
