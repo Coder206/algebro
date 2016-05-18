17e61f2f9d5347fa27e775fff435ebf03bef3ab4
ScryptX <Kyoya@live.ca>
2016-05-18 10:54:11 -0400

    Add files via upload

---
+++ algebro-gh-pages/js/index.js
@@ -0,0 +1,113 @@
+$(document).ready(function () {
+  //Variables - argent
+  var a;
+  var argent;
+  var pointage;
+
+  //Variables - jeu
+  var tours = 3;
+  var numr = 0;
+
+  //DOM
+  a = document.getElementById("a").textContent;
+  sys = document.getElementById("sys").textContent;
+  serie = document.getElementById("serie");
+ //Input argent
+  //Défini argent
+  argent = a;
+  pointage = a * 100;
+
+  if (pointage >= 100) {
+    numr = Math.floor((Math.random() * tours) + 1);
+    nButton();
+  }
+
+  $("body").on("click", "button", function ()
+    //alert("hi");
+    if ($(this).is(".x")) {
+      //alert("X");
+      gagnant();
+      numr = Math.floor((Math.random() * tours) + 0);
+    } else {
+      //alert("O");
+      $(this).remove();
+      perdant();
+      numr = Math.floor((Math.random() * tours) + 0);
+    }
+  function myMoney()
+      var adj = document.getElementById("myText").value;
+      If gagnant();{
+        argent + adj;
+      } else {
+      argent - adj;
+      document.getElementById("demo").innerHTML = var adj ;
+  }
+  });
+  function gagnant() {
+          tours++;
+    //Multiplier la valeur par 0.4
+          nButton();
+    argent = argent * 0.4;
+    //Décision continuer
+    /*var r = confirm("Voulez-vous essayer vos chances encore?");
+    if (r === true) {
+      sys = "Charger";
+            tours++;
+      nButton();
+    } else {
+      sys = "Fermer";
+    }*/
+  }
+
+  function perdant() {
+    //alert("perdant");
+    tours--;
+    rButton();
+    //Multiplier la valeur par 0.45
+    argent = argent * 0.45;
+    //Pas d'argent?
+    /*if (argent <= 0) {
+      sys = "Fermer";
+    } else {
+      //Décision continuer
+      var r = confirm("Voulez-vous essayer vos chances encore?");
+      if (r === rue) {
+        sys = "Charger";
+        //alert(tours)
+        rButton();
+      } else {
+        sys = "Fermer";
+      }
+    }*/
+  }
+
+  function nButton() {
+    $("button").remove();
+   // alert(numr);
+    //alert(tours);
+    for (var i = 0; i < tours; i++) {
+      if (i === numr) {
+        var btn = document.createElement("BUTTON");
+        btn.setAttribute("class", "x");
+        btn.setAttribute("id", "btn");
+    document.body.appendChild(btn);
+        //$(document.createElement('button'));
+        //$('<button id="btn" class="x"></button>').appendTo('#serie');
+      } else {
+        var btn = document.createElement("BUTTON");
+    document.body.appendChild(btn);
+            btn.setAttribute("id", "btn");
+        //$('<button id="btn"></button>').appendTo('#serie');
+        $(document.createElement('button'));
+      }
+
+    }
+  }
+
+  function rButton() {
+    //alert("-1");
+    //$(".x").remove();
+  }
+
+
+});
