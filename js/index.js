61a460b8ece66eee783720fdf59a3243195e1ddd
ScryptX <Kyoya@live.ca>
2016-05-26 17:34:36 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
     }
 
     function pariG() {
-        parseInt(document.getElementById("a").textContent = argent + money);
+        parseInt(a) = parseInt(argent) + parseInt(money)
         alert(a);
     }
 
