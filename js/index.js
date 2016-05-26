76624f871f0e3e54c99d4cf8e15820af984a33d2
Coder206 <patrick.arrow206@outlook.com>
2016-05-26 17:40:16 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
     }
 
     $("body").on("click", "button", function() {
+        alert();
         if ($(this).is(".x")) {
             rnd();
             gagnant();
