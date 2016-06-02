39d34033a388cc6b96e396e94e1ede7c5f3df44f
ScryptX <Kyoya@live.ca>
2016-06-02 08:37:12 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -97,6 +97,6 @@ $(document).ready(function () {
     }
     
     function Va(){
-         document.getElementById("a").textContent = parseInt(document.getElementById("vA").textContent);
+         document.getElementById("Va").textContent = parseInt(document.getElementById("a").textContent);
     }
 });
